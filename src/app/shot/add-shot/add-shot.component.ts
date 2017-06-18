573d43c4fd565f996b41a790759f56b030cc4e1a
Aditya Kurhade <adikrhd@gmail.com>
2017-06-18 17:01:40 +0530

    project_shot Entry done

--- src/app/shot/add-shot/add-shot.component.ts
+++ src/app/shot/add-shot/add-shot.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Shot } from '../shot.interface'
 import { AngularFireDatabase } from 'angularfire2/database'
+import { ProjectService } from '../../shared/cproject.service';
 
 @Component({
     selector: 'add-shot',
@@ -26,7 +27,7 @@ export class AddShotComponent implements OnInit {
 
     public shot: Shot;
 
-    constructor(private db: AngularFireDatabase) { }
+    constructor(private db: AngularFireDatabase,private cproj : ProjectService) { }
 
     ngOnInit() { }
 
@@ -34,5 +35,8 @@ export class AddShotComponent implements OnInit {
         this.shot = form;
         const add = this.db.list('/Shots');
         add.push(this.shot).then(_ => { console.log('Shot added') })
+        const add_project_shot = this.db.object('/Project_Shot/' + this.cproj.getCurrentProjectId());
+
+        add.push(this.shot).then(_ => { add_project_shot.set({ "shot_key": _.key }) });
     }
 }
